cf8f8755c3eac8f55862e8a813af9fed7656e40e
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-03-28 14:21:08 +0530

    Initial Commit

---
+++ run.cmd
@@ -0,0 +1,7 @@
+django-admin startproject todoproject
+cd todoproject
+python manage.py startapp todoapp
+
+
+# Download the Google cloud SQL proxy
+curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
